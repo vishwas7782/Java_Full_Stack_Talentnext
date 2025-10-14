3c5dd75266de30b1708814dd0c7b109c75b999f6
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:47:15 +0530

    Collections: Add Solution6.java (List)

---
+++ 06_Collection/Assignments/List/Solution6.java
@@ -0,0 +1,25 @@
+
+import java.util.Vector;
+
+/*
+ * Implment the assignment 1 using Vector 
+ */
+public class Solution6 {
+    public static void main(String[] args) {
+        Vector<String> months = new Vector<>();
+        months.add("Jan");
+        months.add("Feb");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("Aug");
+        months.add("Sep");
+        months.add("Oct");
+        months.add("Nov");
+        months.add("Dec");
+
+        System.out.println(months);
+    }
+}
