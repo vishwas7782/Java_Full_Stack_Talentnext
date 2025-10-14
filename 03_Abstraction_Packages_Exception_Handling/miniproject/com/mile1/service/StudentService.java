36f8a9bf0c6e70ecc7654397de8dc1545ee788dc
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:12:30 +0530

    Core Java: Add StudentService.java (Mini Project)

---
+++ 03_Abstraction_Packages_Exception_Handling/miniproject/com/mile1/service/StudentService.java
@@ -0,0 +1,47 @@
+package abstraction_packages_exception_handling.miniproject.com.mile1.service;
+
+import abstraction_packages_exception_handling.miniproject.com.mile1.bean.Student;
+
+public class StudentService {
+    // method 
+    // This method is used to count the number of objects
+    // where the marks array is null 
+    public int findNumberOfNullMarksArray ( Student s[] )
+    {
+        try {
+
+        } 
+        catch(NullPointerException e ) {
+
+        }
+        return 0;
+    }    
+
+    public int findNumberOfNullName( Student s[] )
+    {
+        try {
+
+        } 
+        catch(NullPointerException e ) {
+
+        }
+        return 0;   
+    }
+
+    public int findNumberOfNullObjects ( Student s[] ) 
+    {
+        try {
+
+        } 
+        catch(NullPointerException e ) {
+
+        }
+        return 0;
+        
+    }
+
+}
+
+
+
+
