d7a9b858ac35090291fceca8e2f49f93e8eca02f
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:47:15 +0530

    Collections: Add Solution5.java (List)

---
+++ 06_Collection/Assignments/List/Solution5.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+/*
+ * Implement the assignment 1 using Linked List 
+ */
+public class Solution5 {
+    public static void main(String[] args) {
+        LinkedList<String> months = new LinkedList<>();
+        months.add("Jan");
+        months.add("Feb");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("Aug");
+        months.add("Sep");
+        months.add("Oct");
+        months.add("Nov");
+        months.add("Dec");
+
+        System.out.println(months);
+    }
+}
