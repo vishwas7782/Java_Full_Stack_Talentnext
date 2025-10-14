a22192a4723917981d3c066afdf2e7a5dbcdcc97
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:47:14 +0530

    Collections: Add Solution1.java (List)

---
+++ 06_Collection/Assignments/List/Solution1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        ArrayList<String> months = new ArrayList<>();
+        months.add("Jan");
+        months.add("Feb");
+        months.add("March");
+        months.add("April");
+        months.add("May");
+        months.add("June");
+        months.add("July");
+        months.add("Aug");
+        months.add("Sep");
+        months.add("Oct");
+        months.add("Nov");
+        months.add("Dec");
+
+        System.out.println(months);
+    }
+}
