4bbc528a1ead7b1b1d373015f0b050e1da2b3d5e
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:12:29 +0530

    Core Java: Add NullNameException.java (Mini Project)

---
+++ 03_Abstraction_Packages_Exception_Handling/miniproject/com/mile1/exception/NullNameException.java
@@ -0,0 +1,8 @@
+package abstraction_packages_exception_handling.miniproject.com.mile1.exception;
+
+public class NullNameException extends Exception {
+    @Override
+    public String toString() {
+        return "name is null" ;
+    }
+}
