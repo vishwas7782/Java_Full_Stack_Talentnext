7d6517448868f5457b3e8b1f0e3bfc4b6167220e
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:12:30 +0530

    Core Java: Add NullStudentObjectException.java (Mini Project)

---
+++ 03_Abstraction_Packages_Exception_Handling/miniproject/com/mile1/exception/NullStudentObjectException.java
@@ -0,0 +1,8 @@
+package abstraction_packages_exception_handling.miniproject.com.mile1.exception;
+
+public class NullStudentObjectException  extends Exception {
+    @Override
+    public String toString() {
+        return "object is null" ;
+    }
+}
