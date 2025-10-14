d742aef89d819ac729ab0ac17f8664b0e96e37e9
Vishwasjeet Kumar Gupta <vishwas7782@gmail.com>
2025-10-14 16:12:29 +0530

    Core Java: Add NullMarksArrayException.java (Mini Project)

---
+++ 03_Abstraction_Packages_Exception_Handling/miniproject/com/mile1/exception/NullMarksArrayException.java
@@ -0,0 +1,8 @@
+package abstraction_packages_exception_handling.miniproject.com.mile1.exception;
+
+public class NullMarksArrayException  extends Exception {
+    @Override
+    public String toString() {
+        return "mark array is null" ;
+    }
+}
